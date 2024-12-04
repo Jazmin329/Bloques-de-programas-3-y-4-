16e140dfd270a963f9945344f0d0cfe35e89528c
Jazmin329 <jazminmaciassabas@gmail.com>
2024-12-03 18:48:00 -0600

    Create P-1.10.py

---
+++ códigos/P-1.10.py
@@ -0,0 +1,14 @@
+# Programa 1.10 Programa para demostrar el funcionamiento de los métodos upper y lower 
+# Fecha: 2024/11/08
+# Elaborado por: Jazmin Macias Sabas
+
+#Definimos una cadena de texto 
+cadena = "Python es un lenguaje de programación"
+print("\n",cadena)
+
+cadena_mayusculas = cadena.upper()  # Convertimos la cadena a mayúsculas
+print("\n" ,cadena_mayusculas)
+
+cadena_minusculas = cadena.lower() # Convertimos la cadena a mayúsculas
+print("\n" , cadena_minusculas)
+print("\n Fin del programa")
