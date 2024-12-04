58b92f93128bd5053026d5e1574d98b041b791b9
Jazmin329 <jazminmaciassabas@gmail.com>
2024-12-03 18:42:24 -0600

    Create P-1.3.py

---
+++ códigos/P-1.3.py
@@ -0,0 +1,22 @@
+# Programa 1.3 Función rango 
+# Fecha: 2024/11/04
+# Elaborado por: Jazmin Macias Sabas
+print("Imprime los valores del 0 al 9:")
+x = range (10)
+for num in x:
+    print(num)
+
+print("Imprime los valores del 5 al 15:")
+x1 = range (5,16)
+for num in x1:
+    print(num)
+
+print("\nImprime los pares del 10 al 20:")
+x2 = range (10, 21, 2)
+for num in x2:
+    print(num)
+    
+print("\nImprime los impares del 11 al 21:")
+x3 = range (11, 22, 2)
+for num in x3:
+    print(num)
