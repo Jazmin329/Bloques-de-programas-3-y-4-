12fc25333b3b3b5345df32168365767f872ebdf9
Jazmin329 <jazminmaciassabas@gmail.com>
2024-12-03 18:40:48 -0600

    Create P1.1.py

---
+++ códigos/P1.1.py
@@ -0,0 +1,13 @@
+# Programa 1.1 ciclos "for"
+# Fecha: 2024/11/04
+# Elaborado por: Jazmin Macias Sabas
+lista_num = [10, 30, 40, 20, 35, 80]
+print(lista_num [0])
+print(lista_num [1])
+print(lista_num [2])
+print(lista_num [3])
+print(lista_num [4])
+print(lista_num [5])
+# Haciendo lo mismo pero con un ciclo "for"
+for i in lista_num:
+    print(i)
