162f48714603952a5aaf96559114145069828cc4
Jazmin329 <jazminmaciassabas@gmail.com>
2024-12-03 18:38:38 -0600

    Create P-11.py

---
+++ códigos/P-11.py
@@ -0,0 +1,7 @@
+# Programa 11 Agregar elementos a una lista
+# Fecha: 2024/10/31
+# Elaborado por: Jazmin Macias Sabas
+colores = ["rojo", "azul"]
+print(colores)
+colores.append("verde")
+print(colores)
