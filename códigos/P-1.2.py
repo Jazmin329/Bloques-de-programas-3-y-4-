2ee0503f66145a8704ce79d444e3db0c5aac7128
Jazmin329 <jazminmaciassabas@gmail.com>
2024-12-03 18:41:27 -0600

    Create P-1.2.py

---
+++ códigos/P-1.2.py
@@ -0,0 +1,6 @@
+# Programa 1.2 programa que imprima los nombres 
+# Fecha: 2024/11/04
+# Elaborado por: Jazmin Macias Sabas
+nombres = ["Luis", "Chuy", "Mauricio"]
+for nombre in nombres: 
+    print("El nombre es: ",nombre)
