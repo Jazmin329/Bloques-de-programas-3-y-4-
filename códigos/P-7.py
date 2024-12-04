bce842ec9ef6af80a2063ad787b4097dd3dd8fee
Jazmin329 <jazminmaciassabas@gmail.com>
2024-12-03 18:36:19 -0600

    Create P-7.py

---
+++ códigos/P-7.py
@@ -0,0 +1,9 @@
+# Programa 7 Tipos de datos Lista y tipos de datos de los elementos 
+# Fecha: 2024/10/30
+# Elaborado por: Jazmin Macias Sabas 
+cadenas = ["Juan", "Pedro", "Chuy"]
+print(cadenas)
+print(cadenas[1])
+print(type(cadenas))
+print(type(cadenas[1]))
+
