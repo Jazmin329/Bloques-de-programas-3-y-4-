18481df0a2e1b7a29c56ab4f4df2899a0d749e0d
Jazmin329 <jazminmaciassabas@gmail.com>
2024-12-03 18:43:01 -0600

    Create P-1.4.py

---
+++ códigos/P-1.4.py
@@ -0,0 +1,13 @@
+#Programa 1.4 Programa que imprima el nombre de mis 10 personajes favoritos
+# Fecha: 2024/11/04
+# Elaborado por: Jazmin Macias Sabas 
+
+
+personajes = ["Nick","spiderman","Loki","Flynn Rider","Adam Carlsen","Venom","Alexander","Hades","El genio de la lámpara","Patrick Verona"]
+
+# Imprimir cada personaje en la lista
+print("Mis 10 personajes favoritos son:")
+for personajes in personajes:
+    print(personajes)
+
+print("Gracias por ver mis 10 personajes favoritos")
