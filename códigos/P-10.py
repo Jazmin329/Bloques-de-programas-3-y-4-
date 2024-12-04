4999ba45c0c898a859859b8191ebb386d1edd3cd
Jazmin329 <jazminmaciassabas@gmail.com>
2024-12-03 18:38:02 -0600

    Create P-10.py

---
+++ códigos/P-10.py
@@ -0,0 +1,8 @@
+# Programa 10 List Membership
+# Fecha: 2024/10/31
+# Elaborado por: Jazmin Macias Sabas
+nombres = ["Choto", "Emilio", "Luis"]
+print("Luis" in nombres) #True
+print("Emi" in nombres) #False
+print("Javier" in nombres) #False
+print("Jose" not in nombres) #True
