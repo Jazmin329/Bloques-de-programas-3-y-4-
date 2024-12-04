2c201967458c24c63f0d8fecf2d057472d5bd23d
Jazmin329 <jazminmaciassabas@gmail.com>
2024-12-03 18:33:53 -0600

    Create P-3.py

---
+++ códigos/P-3.py
@@ -0,0 +1,11 @@
+# Programa 3 Identificación de tipos de datos str, int y float 
+# Fecha: 2024/10/28
+# Elaborado por: Jazmin Macias Sabas 
+variable = input("Ingresa tu edad: ")
+print(type(variable))
+
+variable = int(variable) #Convertir la variable a tipo entero 
+print(type(variable))
+
+variable = float(variable) #Convertir la variable a tipo flotante 
+print(type(variable))
